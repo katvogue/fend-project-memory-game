b0f04e21b5c8506ecaadd24961a2ab1eab1c4092
Your Name <you@example.com>
2018-08-26 22:24:48 -0400

    Added list of all cards in js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
+const cardList = ["fa fa-diamond", "fa fa-diamond", "fa fa-anchor", "fa fa-bolt", "fa fa-cube","fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"]
+
 
 
 /*
